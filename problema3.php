8bb21a6b756182a0248183d29083f7150ba1a12b
Shirley 🥯 <shirleywen1@gmail.com>
2026-06-10 11:13:09 -0500

    cambios

--- problema3.php
+++ problema3.php
@@ -4,19 +4,8 @@ require_once 'Utilidades.php';
 // Calculamos la suma con un bucle for
 $suma = 0;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $n = filter_var($_POST['n'] ?? '', FILTER_VALIDATE_INT, [
-        'options' => ['min_range' => 1, 'max_range' => 500]
-    ]);
-
-    if ($n === false) {
-        $errores[] = "Ingresa un número entero entre 1 y 500.";
-    } else {
-        for ($i = 1; $i <= $n; $i++) {
-            $multiplos[] = 4 * $i;
-        }
-    }
+for ($i = 1; $i <= 1000; $i++) {
+    $suma += $i;
 }
 ?>
 
@@ -29,7 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <table>
         <tr><th>Rango</th>        <td>Del 1 al 1,000</td></tr>
-        <tr><th>Estructura</th>   <td>for ($i = 1; $i &lt;= 1000; $i++)</td></tr>
         <tr><th>Resultado</th>    <td><strong><?= number_format($suma) ?></strong></td></tr>
     </table>
 </div>
